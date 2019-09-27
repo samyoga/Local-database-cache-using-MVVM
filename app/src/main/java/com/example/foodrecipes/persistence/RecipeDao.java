0ddab47dd381cc83193a8c761d6ff4fe43cf5cc8
samyoga <bhattaraisamyoga@gmail.com>
2019-09-27 15:26:38 +0545

    Created interface Room Dao

---
+++ app/src/main/java/com/example/foodrecipes/persistence/RecipeDao.java
@@ -0,0 +1,36 @@
+package com.example.foodrecipes.persistence;
+
+import android.arch.lifecycle.LiveData;
+
+import androidx.room.Dao;
+import androidx.room.Ignore;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.foodrecipes.models.Recipe;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.IGNORE;
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface RecipeDao {
+
+    @Insert(onConflict = IGNORE)
+    long[] insertRecipes(Recipe... recipe);
+
+    @Insert(onConflict = REPLACE)
+    void insertRecipe(Recipe recipe);
+
+    @Query("UPDATE recipes SET title = :title, publisher = :publisher, image_url = :image_url, social_rank = :social_rank" +
+    " WHERE recipe_id = recipe_id")
+    void updateRecipe(String recipe_id, String title, String publisher, String image_url, float social_rank);
+
+    @Query("SELECT * FROM recipes WHERE title LIKE '%' || :query || '%' OR ingredients LIKE '%' || :query || '%' " +
+            "ORDER By social_rank DESC LIMIT (:pageNumber * 30)")
+    LiveData<List<Recipe>> searchRecipes(String query, int pageNumber);
+
+    @Query("SELECT * FROM recipes WHERE recipe_id = recipe_id")
+    LiveData<Recipe> getRecipe(String recipe_id);
+}
